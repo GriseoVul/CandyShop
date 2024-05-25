eb3fba7683ce81c345cfcc62146e03d423110578
GriseoVul <off.sergienko@gmail.com>
2024-05-25 22:47:10 +0300

    Added db sets

--- src/CandyShop.API/Data/ApplicationDbContext.cs
+++ src/CandyShop.API/Data/ApplicationDbContext.cs
@@ -1,7 +1,17 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using CandyShop.API.Models;
 namespace CandyShop.API.Data;
 
-public class ApplicationDbContext : DbContext
-{
 
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+{
+    public DbSet<User> Users{ get; set; }
+    public DbSet<Order> Orders{ get; set; }
+    public DbSet<Basket> Baskets{ get; set; }
+    public DbSet<Product> Products{ get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        //create table connections
+    }
 }
