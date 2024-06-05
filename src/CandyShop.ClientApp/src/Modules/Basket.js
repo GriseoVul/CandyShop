b75870b17309f51675a4db63cc9d555ab85e5389
StoneZol <miha2000rus@gmail.com>
2024-06-06 00:00:28 +0300

    rebuil items

--- src/CandyShop.ClientApp/src/Modules/Basket.js
+++ src/CandyShop.ClientApp/src/Modules/Basket.js
@@ -4,105 +4,15 @@ import ProductCard from './ProductCard';
 import Swal from 'sweetalert2';
 
 const Basket = () => {
-  const { basketItems, setBasketItems } = useContext(MyContext);
+  const { basketItems, setBasketItems, removeFromBasket} = useContext(MyContext);
   const [basketTotal, setBasketTotal] = useState(0);
   useEffect(() => {
     const total = basketItems.reduce((sum, item)=> sum += item.totalprice , 0)
     setBasketTotal(total);
-  }, [basketItems, setBasketItems])
-
-  const removeItemFromBasket = (itemToRemove) => {
-    setBasketItems(basketItems.filter(item => item.id !== itemToRemove.id));
-  };
-
-  const showEditQuantityAlert = (item) => {
-    let quantity = item.quantity;
-    let totalprice = item.totalprice;
-    let saleprice = item.price-item.price/100*item.sale
+  }, [basketItems, setBasketItems,removeFromBasket])
 
 
-    Swal.fire({
-      title: item.name,
-      html: `
-        <div style="display: flex; flex-direction: column; align-items: center; width: 100%; max-width: 500px; margin: auto;">
-          <img src="${item.pic}" alt="image" style="width: 100%; max-width: 300px; height: auto; border-radius: 10px;">
-          <p style="text-align: center; margin: 10px 0;">${item.description}</p>
-          <p id="price" style="text-align: center; font-size: 1.2em; font-weight: bold;">₽${item.price}</p>
-          <p id="sale-price" style="text-align: center; font-size: 1.2em; font-weight: bold;">₽${saleprice}</p>
-          <p id="total-price" style="text-align: center; font-size: 1em;">Всего ₽${item.totalprice}</p>
-          <div style="display: flex; justify-content: center; align-items: center; margin-top: 20px; width: 100%;">
-            <button class="button" id="decrease" >-</button>
-            <input id="quantity" type="number" value="${item.quantity}" min="1" max="999" style="width: 60px; text-align: center; margin: 0 10px; padding: 6px; border: 1px solid #ccc; border-radius: 4px;">
-            <button class="button" id="increase">+</button>
-          </div>
-          <button class="button" id="update" >Обновить</button>
-        </div>
-      `,
-      showConfirmButton: false,
-      didOpen: () => {
-        const quantityInput = document.getElementById('quantity');
-        const totalPriceElement = document.getElementById('total-price');
-        const saleP = document.getElementById('sale-price')
-        const priceP = document.getElementById('price')
-
-        if (item.sale === 0){
-          saleP.style = "display: none"
-        } else {
-          priceP.style = "text-decoration: line-through"
-          Object.assign(saleP.style, {
-            color: 'red',
-            fontSize: "1.5em",
-            margin: '5px'
-          })
-        }
-
-        const updateTotalPrice = () => {
-          if (item.sale > 0){
-            totalprice= quantity*saleprice;
-            } else {totalprice= quantity*item.price;}
-          totalPriceElement.textContent=`Всего ₽${totalprice}`;
-        };
-        updateTotalPrice();
-
-        document.getElementById('increase').addEventListener('click', () => {
-          let currentValue = parseInt(quantityInput.value);
-          if (currentValue < 999) {
-            quantityInput.value = currentValue + 1;
-            quantity = quantityInput.value;
-            updateTotalPrice();
-          }
-        });
-
-        document.getElementById('decrease').addEventListener('click', () => {
-          if (parseInt(quantityInput.value) > 1) {
-            quantityInput.value = parseInt(quantityInput.value) - 1;
-            quantity = quantityInput.value;
-            updateTotalPrice();
-          }
-        });
-
-        quantityInput.addEventListener('input', () => {
-          if (quantityInput.value < 1) {
-            quantityInput.value = 1;
-          } else if (quantityInput.value > 999) {
-            quantityInput.value = 999;
-          }
-          quantity = quantityInput.value;
-          updateTotalPrice();
-        });
-
-        document.getElementById('update').addEventListener('click', () => {
-          const updatedItem = { ...item, quantity: parseInt(quantityInput.value), totalprice };
-
-          setBasketItems(basketItems.map(basketItem => 
-            basketItem.id === item.id ? updatedItem : basketItem
-          ));
-
-          Swal.fire(`Обновлено`);
-        });
-      }
-    });
-  };
+  
   return (
     <>
     <h2 style={{textAlign: 'center', marginTop: "45px"}}>Корзина</h2>
@@ -110,17 +20,13 @@ const Basket = () => {
       {basketItems.length === 0 ? (
         <p>Корзина пуста</p>
       ) : (
-        <div className='CandysBasket'>
+        <div className='Candys'>
+          
           {basketItems.map((item) => (
-            <ProductCard
-              key={item.id} // Используем уникальный идентификатор вместо индекса
-              item={item}
-              onButtonClick={() => showEditQuantityAlert(item)}
-              onRemoveClick={() => removeItemFromBasket(item)}
-              buttonText="Изменить"
-              showQuantity={true}
-              basket={true}
-            />
+           <div className='Candy'> <ProductCard
+            key={item.id}
+            item={item}
+            /> </div>
           ))}
         </div>
       )}
--- src/CandyShop.ClientApp/src/Modules/ProductCard.js
+++ src/CandyShop.ClientApp/src/Modules/ProductCard.js
@@ -1,34 +1,104 @@
-import React from 'react';
+import React, {useState, useEffect ,useContext} from 'react';
+import { MyContext } from './MyContext';
+import Swal from 'sweetalert2';
+import Dec from './Pictures/dec.png'
+import In from './Pictures/in.png'
 
 const ProductCard = ({ item, onButtonClick, onRemoveClick, buttonText, showQuantity, basket }) => {
-  const yesbasket  = (<div className='basket-item'>
-  <img src={item.pic} alt={item.name} />
-  <div className='basket-list'>
-    <h3>{item.name}</h3>
-    <span className='price'>₽{item.price}</span>
-    {item.sale > 0 && <span className='sale-price'> -{item.sale}%</span>}
-    {item.sale === 0 && <span className='sale-price' style={{visibility: 'hidden'}}>Пусто</span>}
-    {showQuantity && <p>Количество: {item.quantity}</p>}
-    <button className='button' onClick={onButtonClick}>{buttonText}</button>
-    {onRemoveClick && (
-      <button className='button' onClick={onRemoveClick} style={{ marginLeft: '10px' }}>X</button>
-    )}
-  </div>
-  </div>)
+  const [isQuantity, setIsQuantity] = useState(0)
+  const { addToBasket, basketItems, removeFromBasket } = useContext(MyContext)
+useEffect(()=> {
+  const existingItem = basketItems.find(basketItem => basketItem.id === item.id)
+  if (existingItem){
+    setIsQuantity(existingItem.quantity)
+  }
+},[basketItems, item.id])
 
-  const noBasket = ( <div className='product-item'>
-  <img src={item.pic} alt={item.name} />
-  <div className='product-list'>
-    <h3>{item.name}</h3>
-    <span className='price'>₽{item.price}</span>
-    {item.sale > 0 && <span className='sale-price'> -{item.sale}%</span>}
-    {item.sale === 0 && <span className='sale-price' style={{visibility: 'hidden'}}>Пусто</span>}
-    {showQuantity && <p>Количество: {item.quantity}</p>}
-    <button className='button' onClick={onButtonClick}>{buttonText}</button>
-  </div>
-</div>)
-  return basket === true ? yesbasket : noBasket;
+const handlerZeroState = () => {
+  setIsQuantity(0)
+  removeFromBasket(item)
+ } 
 
+const handlerAddToBasket = () => {
+  const newItem = {...item, quantity: isQuantity+1}
+  addToBasket(newItem)
+}
+const handlerRemoveFromBasket = () => {
+  if (isQuantity >1 ){
+    const newItem = {...item, quantity: isQuantity-1}
+    addToBasket(newItem)
+  } 
+ else handlerZeroState()
+  }
+
+  const handleInputChange = (e) => {
+    const value = parseInt(e.target.value, 10);
+    if (!isNaN(value) && value >= 0 && value <= 999) {
+      if (value === 0) {
+        handlerZeroState();
+      } 
+      else {
+        setIsQuantity(value);
+        const newItem = { ...item, quantity: value };
+        addToBasket(newItem);
+      }
+    }
+  };
+  const showProductAlert = () => {
+      Swal.fire({
+        title: item.name,
+        html: `
+        <div style="display: flex; flex-direction: column; align-items: center; width: 100%; max-width: 500px; margin: auto;">
+        <img src="${item.pic}" alt="image" style="width: 100%; max-width: 300px; height: auto; border-radius: 10px;">
+        <p style="text-align: center; margin: 10px 0;">${item.description}</p>
+      </div>
+        `,
+        showConfirmButton: false,
+      });
+    };
+
+  function handlerClick(event){
+      event.preventDefault();
+      showProductAlert()
+  }
+
+const noemptis = (<>
+  <button className="button"onClick={handlerAddToBasket}>В корзину</button>
+</>)
+const emptis = (<>
+
+  <button className="button" onClick={handlerRemoveFromBasket}>-</button>
+  <input className="input-quantity" type="number" value={isQuantity} min="0" max="999" onChange={handleInputChange}/>
+  <button className="button" onClick={handlerAddToBasket} >+</button> <br/>
+  <button className="button" onClick={handlerZeroState}>Удалить</button>
+</>)
+return (
+<div className='product-item'>
+    <img src={item.pic} alt={item.name} onClick={handlerClick}/>
+    <div className='product-list'>
+        <h3>{item.name}</h3>
+        <span className='price'>₽{item.price}</span>
+        {item.sale > 0 && <span className='sale-price'>-{item.sale}%</span>} 
+        {item.sale === 0 && <span className='sale-price' style={{visibility: 'hidden'}}>Пусто</span>}<br/>
+        {isQuantity === 0 ? (
+          <button className="button" onClick={handlerAddToBasket}>В корзину</button>
+        ) : (
+          <div className="quantity-controls">
+            <button className="button" onClick={handlerRemoveFromBasket}>-</button>
+            <input
+              className="input-quantity"
+              type="number"
+              value={isQuantity}
+              min="0"
+              max="999"
+              onChange={handleInputChange}/>
+            <button className="button" onClick={handlerAddToBasket}>+</button><br/>
+            <button className="button" onClick={handlerZeroState}>Удалить</button>
+          </div>
+        )}
+    </div>
+</div>
+)
 };
 
 export default ProductCard;
