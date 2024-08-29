ce05f44b23d403ee26c953814f0139f99d231ac0
StoneZol <miha2000rus@gmail.com>
2024-08-29 14:08:00 +0300

    add paginator in Candys

--- src/CandyShop.ClientApp/src/Modules/Candys.js
+++ src/CandyShop.ClientApp/src/Modules/Candys.js
@@ -1,11 +1,16 @@
 import React , {useEffect, useState} from 'react';
 import Candy from './Candy';
-import Slider from './Slider';
+import ResponsivePagination from 'react-responsive-pagination'
+import { dropEllipsis } from 'react-responsive-pagination/narrowBehaviour';
 import { isUrl } from './MyContext';
 
 const Candys = () => {
     const url = `${isUrl}/Product` //api/Product //${isUrl}/Product
     const [candysItem, setCandysItem] = useState([])
+    const [currentPage, setCurrentPage] = useState(1)
+    const [itemsPerPage] = useState(2) //Количкество товаров на странице
+
+
     useEffect(()=> {
         const fetchData = async() => {
             try{
@@ -21,13 +26,13 @@ const Candys = () => {
         fetchData()
     }, [])
 
-    const handleScrollToTop = () => {
+    const handleScrollToTop = () => { // перемотка к началу страницы
         window.scrollTo({
             top: 0,
             behavior: 'smooth'
         });
     };
-    
+
     async function getCandysData(){
         try {
             const responce = await fetch(url, {
@@ -45,17 +50,35 @@ const Candys = () => {
             throw new Error(error.message)
         }
     }
+
+    // пагинатор
+    const indexOfLastItem = currentPage * itemsPerPage;
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+    const currentItems = candysItem.slice(indexOfFirstItem, indexOfLastItem)
+
     return (
         <>
         <div className='CandysBox'>
         <div className='Candys'>
-   {candysItem.length > 0 ? (candysItem.map((candy, index) => (
+   {currentItems.length > 0 ? (currentItems.map((candy, index) => (
        <Candy key={index} item={candy} />
    ))): <p>Загрузка...</p>}
    {candysItem.length>0 && <button className={`scroll-to-top button`} style={{opacity: "50%"}} onClick={handleScrollToTop}>Вверх</button>}
         </div> 
 
         </div>
+        <div className='paginationBox'>
+        <ResponsivePagination 
+            current={currentPage}
+            total={Math.ceil(candysItem.length / itemsPerPage)}
+            onPageChange={page => {
+                setCurrentPage(page);
+                handleScrollToTop();
+                }
+            }
+         
+        />
+        </div><br/><br/>
         </>
     );
 }
---
+++ src/CandyShop.ClientApp/src/Modules/ClientPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Slider from './Slider';  
+import Candys from './Candys';
+const ClientPage = () => {
+    return (
+        <div>
+            <Slider/>
+            <Candys/>
+        </div>
+    );
+}
+
+export default ClientPage;
